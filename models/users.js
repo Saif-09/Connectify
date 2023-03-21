0d10a81094cc974200cf70a81bfd28d4dd48e6ae
Saif-09 <saifmd238@gmail.com>
2023-03-21 17:14:17 +0530

    created model of user schema

---
+++ models/users.js
@@ -0,0 +1,22 @@
+const mongoose= require('mongoose');
+
+const userSchema = new mongoose.userSchema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    }
+   
+},{timestamps:true
+});
+
+const User = mongoose.model('User', userSchema);
+model.exports = User;
