b73633995fcdacc18fd4667a34b50e0a7f3dd3e8
Saif-09 <saifmd238@gmail.com>
2023-03-21 11:36:02 +0530

    created style

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app =  express();
 const port = 80;
+const expressLayouts = require('express-ejs-layouts');
 const routes = require('./routes');
 
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+//extract styles and scripts of subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 //use express router
 app.use('/',routes);
 
