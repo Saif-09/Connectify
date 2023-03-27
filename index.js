768071196dc7273db68a9d23e1aa3c26fc7e5dd4
Saif-09 <saifmd238@gmail.com>
2023-03-27 06:02:06 +0530

    set authentication

--- index.js
+++ index.js
@@ -38,8 +38,11 @@ app.use(session({
   }
 }));
 
-app.use(passport.session());
+
 app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // use express router
 app.use('/', require('./routes'));
