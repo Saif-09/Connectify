a1fc7c90da089e33a0cde17e9c7be563aef44678
Saif-09 <saifmd238@gmail.com>
2023-04-07 07:50:43 +0530

    rendered the user info

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,14 @@
-module.exports.home = (req,res)=>{
-   
-    return res.render('home',{
-        title:"Connectify"
+const Post = require("../models/post");
+
+module.exports.home = async (req, res)=> {
+  try {
+    const posts = await Post.find({}).populate('user').exec();
+    return res.render("home", {
+      title: "Connectify",
+      posts: posts
     });
-}
+  } catch (err) {
+    console.log("Error in fetching posts: ", err);
+    return res.redirect("back");
+  }
+};
