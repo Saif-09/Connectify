a222ff397e2220e8b32138de36686bb27ffe311a
Saif-09 <saifmd238@gmail.com>
2023-04-07 05:51:47 +0530

    moving towards creating post js

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const store = MongoStore.create({
   mongoUrl: 'mongodb://127.0.0.1:27017/connectify_db',
   ttl: 24 * 60 * 60 // 1 day
 });
+const sassMiddleware =require('node-sass-middleware');
+
+app.use(sassMiddleware({
+  src:'./assets/scss',
+  dest:'./assets/css',
+  debug: true,
+  outputStyle: 'extended',
+  prefix: '/css',
+}));
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
